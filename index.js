1086865a6b150de8511c2be776e93ecd954acc4c
Yukiniro <yukiniro@hotmail.com>
2021-07-05 22:13:05 +0800

    初始化并提供下载接口

---
+++ index.js
@@ -0,0 +1,39 @@
+export default (function () {
+
+  function createLink() {
+    let a = document.createElement('a');
+    document.body.appendChild(a);
+    a.style.display = 'none';
+    return a;
+  }
+
+  return {
+
+    /**
+     * @description 下载
+     * @param source <string | file | blob>
+     * @param options
+     * @param options.name
+     */
+    download: function(source, options = {name: ''}) {
+      const {name} = options;
+      const a = createLink();
+      let url;
+      const useURL = false;
+      if (typeof source === 'string') {
+        url = source;
+      } else {
+        url = URL.createObjectURL(source);
+        useURL = true;
+      }
+      
+      a.href = url;
+      a.download = name;
+      a.click();
+      
+      if (useURL) {
+        URL.revokeObjectURL(url);
+      }
+    }
+  }
+})();
